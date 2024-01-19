fb83a4e95c08af16435c38a62d688d4cb920425a
MegeKaplan <kaplanmehmetege@gmail.com>
2024-01-19 17:55:54 +0300

    added firebase config improvement and .gitignore file

--- app/components/Home.js
+++ app/components/Home.js
@@ -9,16 +9,8 @@ import { createPage } from "../js/createPage.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDzapiK0GVV_tn4mOiP7AdQFgLyH_V-GrY",
-    authDomain: "snappix-9b5f4.firebaseapp.com",
-    databaseURL: "https://snappix-9b5f4-default-rtdb.firebaseio.com",
-    projectId: "snappix-9b5f4",
-    storageBucket: "snappix-9b5f4.appspot.com",
-    messagingSenderId: "639424361399",
-    appId: "1:639424361399:web:c819bc884fc5890542ee32"
-};
+// Firebase Config
+import { firebaseConfig } from "../db/config.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- app/js/auth.js
+++ app/js/auth.js
@@ -3,19 +3,8 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore, doc, setDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDzapiK0GVV_tn4mOiP7AdQFgLyH_V-GrY",
-  authDomain: "snappix-9b5f4.firebaseapp.com",
-  databaseURL: "https://snappix-9b5f4-default-rtdb.firebaseio.com",
-  projectId: "snappix-9b5f4",
-  storageBucket: "snappix-9b5f4.appspot.com",
-  messagingSenderId: "639424361399",
-  appId: "1:639424361399:web:c819bc884fc5890542ee32"
-};
+// Firebase Config
+import { firebaseConfig } from "../db/config.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -25,7 +14,6 @@ const auth = getAuth(app);
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
-
 
 
 // Get Form Data
